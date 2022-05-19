b16b6bebbc25dca765eb24a8237b325ffdd9f4ad
IqyGod <99662300+IqyGod@users.noreply.github.com>
2022-05-19 15:22:08 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+
+const Qsound = document.getElementById("Q")
+function playSoundQ() {
+  Qsound.play();
+} 
+const Wsound = document.getElementById("W")
+function playSoundW() {
+  Wsound.play();
+}
+const Esound = document.getElementById("E")
+function playSoundE() {
+  Esound.play();
+} 
+const Asound = document.getElementById("A")
+function playSoundA() {
+  Asound.play();
+}
+const Ssound = document.getElementById("S")
+function playSoundS() {
+  Ssound.play();
+}
+const Dsound = document.getElementById("D")
+function playSoundD() {
+  Dsound.play();
+} 
+const Zsound = document.getElementById("Z")
+function playSoundZ() {
+  Zsound.play();
+} 
+const Xsound = document.getElementById("X")
+function playSoundX() {
+  Xsound.play();
+} 
+const Csound = document.getElementById("C")
+function playSoundC() {
+  Csound.play();
+} 
